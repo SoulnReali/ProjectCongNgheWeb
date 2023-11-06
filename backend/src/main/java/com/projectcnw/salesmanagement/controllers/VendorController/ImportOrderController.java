5a06bc67e41e49d8aa6c1d86840cb4ada6dda2c5
Nguyễn Quỳnh Anh <anh.nq170038@sis.hust.edu.vn>
2023-11-06 19:01:44 +0700

    Add getImportDetail in ImportOrderController

--- backend/src/main/java/com/projectcnw/salesmanagement/controllers/VendorController/ImportOrderController.java
+++ backend/src/main/java/com/projectcnw/salesmanagement/controllers/VendorController/ImportOrderController.java
@@ -15,14 +15,14 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/admin/import")
 public class ImportOrderController {
-    private ImportOrderService importOrderService;
+    private final ImportOrderService importOrderService;
 
-    ImportOrderController(ImportOrderService importOrderService){
+    ImportOrderController(ImportOrderService importOrderService) {
         this.importOrderService = importOrderService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<ResponseObject> getImportOrderList(){
+    public ResponseEntity<ResponseObject> getImportOrderList() {
         List<ImportOrderDTO> importOrderDTOList = importOrderService.findAll();
         return ResponseEntity.ok(ResponseObject.builder()
                 .responseCode(200)
@@ -30,16 +30,14 @@ public class ImportOrderController {
                 .data(importOrderDTOList)
                 .build());
     }
-    @RequestMapping(method = RequestMethod.GET, params = {"name","phone"})
+
+    @RequestMapping(method = RequestMethod.GET, params = {"name", "phone"})
     public ResponseEntity<ResponseObject> searchImport(@RequestParam(value = "name", defaultValue = "null") @Valid String name,
                                                        @RequestParam(value = "phone", defaultValue = "null") @Valid String phone) {
-        List<ImportOrderDTO> importOrderDTOList = importOrderService.findAll();;
-        if(!Objects.equals(name, "null"))
-        {
-           importOrderDTOList = importOrderService.findByName(name);
-        }
-        else if(!Objects.equals(phone, "null"))
-        {
+        List<ImportOrderDTO> importOrderDTOList = importOrderService.findAll();
+        if (!Objects.equals(name, "null")) {
+            importOrderDTOList = importOrderService.findByName(name);
+        } else if (!Objects.equals(phone, "null")) {
             importOrderDTOList = importOrderService.findByPhone(phone);
         }
         return ResponseEntity.ok(ResponseObject.builder()
@@ -47,7 +45,16 @@ public class ImportOrderController {
                 .message("Success")
                 .data(importOrderDTOList)
                 .build());
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseObject> getImportDetail(@PathVariable("id") int orderId) {
+        ImportOrderDTO importOrderDTOList = importOrderService.findById(orderId);
 
-}
+        return ResponseEntity.ok(ResponseObject.builder()
+                .responseCode(200)
+                .message("Success")
+                .data(importOrderDTOList)
+                .build());
+    }
 }
